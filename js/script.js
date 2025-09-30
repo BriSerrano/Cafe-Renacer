84db4e8ed848c24b96311e465438b658d1b78e02
Brithany Serrano <serranobri71@gmail.com>
2025-09-29 19:57:39 -0600

    cambiooos

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@
     document.getElementById("toggle-dark").addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
     });
+const toggleButton = document.getElementById('darkModeToggle');
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    // Cambiar ícono del botón
+    if(document.body.classList.contains('dark-mode')){
+        toggleButton.textContent = '☀️';
+    } else {
+        toggleButton.textContent = '🌙';
+    }
+});
